b355f6fa5419b02c48f0568b9f7a4666668ac7b6
Jason Cluck <jcluck@gwmail.gwu.edu>
2012-12-01 23:55:37 -0500

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixins::Application.config.secret_token = '4578c00fcfd0eac6dc1f0fd94409e7aac61354d7021af274b47f105a4f071cc9e9ac37e64dfe85d36bc99e14d1c244fca2922a53d94465bfaa4b6fb1208209ce'
